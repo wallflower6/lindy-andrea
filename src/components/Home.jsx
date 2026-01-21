51eb0c9be17188ad47a6242e879b7a3613a290f7
wallflower6 <lindylimlw@gmail.com>
2026-01-21 20:54:23 +0800

    Updated homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import Chijmes from "../assets/Chijmes.jpg"
 
 const Home = () => {
   const { t, i18n } = useTranslation();
@@ -6,6 +7,17 @@ const Home = () => {
   return (
     <div className="home">
       <h2>{t("home.homepage")}</h2>
+      <br />
+      <p>{t("home.content_0")}</p>
+
+      <figure className="image-figure">
+      <img src={Chijmes} className="home-image" />
+      <figcaption className="image-caption">
+        Photo Credit: Tran Linh Chi
+      </figcaption>
+    </figure>
+    
+      
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import DoubleHappiness_v0 from "../assets/DoubleHappiness_v0.png"
 
 export default function Navbar() {
   const { t, i18n } = useTranslation();
@@ -14,13 +15,18 @@ export default function Navbar() {
 
   return (
     <nav className="navbar">
-      <a href="/"><h1>A+L</h1></a>
+      {/* <a href="/"><h1>A+L</h1></a> */}
+      <a href="/" className="brand">
+        <img src={DoubleHappiness_v0} alt="A+L logo" className="logo" />
+        <h1>A+L</h1>
+      </a>
+
       <div className="links">
         <a href="/venue">{t("nav.venue")}</a>
         <a href="/program">{t("nav.program")}</a>
         <a href="/rsvp" style={{ 
           color: 'white', 
-          backgroundColor: '#f1356d',
+          backgroundColor: '#750D37',
           borderRadius: '8px' 
         }}>RSVP</a>
       </div>
