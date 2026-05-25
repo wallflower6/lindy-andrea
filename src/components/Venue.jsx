6f75cda824a4ea60799e6650b5a2e8330d111179
wallflower6 <lindylimlw@gmail.com>
2026-05-25 21:45:39 +0800

    Updated MXP Terminal 1 directions

--- src/components/Venue.jsx
+++ src/components/Venue.jsx
@@ -1,4 +1,11 @@
 import { useTranslation } from "react-i18next";
+import mxp_1 from "../assets/mxp_1.jpg"
+import mxp_2 from "../assets/mxp_2.jpg"
+import mxp_3 from "../assets/mxp_3.jpg"
+import mxp_4 from "../assets/mxp_4.jpg"
+import mxp_5 from "../assets/mxp_5.jpg"
+import mxp_6 from "../assets/mxp_6.jpg"
+import mxp_7 from "../assets/mxp_7.jpg"
 
 const Venue = () => {
   const { t, i18n } = useTranslation();
@@ -40,7 +47,64 @@ const Venue = () => {
           <div className="venue_transport_air_option">
             <h4>{t("venue.venue_transport.air.milan_airport")}</h4>
             <p>{t("venue.venue_transport.air.milan_airport_p0")}</p>
+            <figure className="image-figure">
+              <img src={mxp_1} className="home-image" />
+              <figcaption className="image-caption">
+                STEP 1: Take the lift from Arrivals between Doors 4 and 6 to Floor -1, and follow the signs until you reach this
+              </figcaption>
+            </figure>
             <br />
+            <p>{t("venue.venue_transport.air.milan_airport_p1")}</p>
+            <figure className="image-figure">
+              <img src={mxp_2} className="home-image" />
+              <figcaption className="image-caption">
+                STEP 2: Keep walking until you see this "Porta Milano"
+              </figcaption>
+            </figure>
+            <br />
+            <p>{t("venue.venue_transport.air.milan_airport_p2")}</p>
+            <figure className="image-figure">
+              <img src={mxp_3} className="home-image" />
+              <figcaption className="image-caption">
+                STEP 3: Keep going straight, and when you see this, follow the sign for 'Airport bus'"
+              </figcaption>
+            </figure>
+            <br />
+            <p>{t("venue.venue_transport.air.milan_airport_p3")}</p>
+            <figure className="image-figure">
+              <img src={mxp_4} className="home-image" />
+              <figcaption className="image-caption">
+                STEP 4: Continue following the sign for 'Airport bus'
+              </figcaption>
+            </figure>
+            <br />
+            <p>{t("venue.venue_transport.air.milan_airport_p4")}</p>
+            <figure className="image-figure">
+              <img src={mxp_5} className="home-image" />
+              <figcaption className="image-caption">
+                STEP 5: When you exit the building into this place, TURN LEFT (as the sign shows)
+              </figcaption>
+            </figure>
+            <br />
+            <p>{t("venue.venue_transport.air.milan_airport_p5")}</p>
+            <figure className="image-figure">
+              <img src={mxp_6} className="home-image" />
+              <figcaption className="image-caption">
+                STEP 6: After turning left, keep walking straight, the buses are just ahead
+              </figcaption>
+            </figure>
+            <br />
+            <p>{t("venue.venue_transport.air.milan_airport_p6")}</p>
+            <figure className="image-figure">
+              <img src={mxp_7} className="home-image" />
+              <figcaption className="image-caption">
+                STEP 7: Bus stands are here! Find the one that goes to Turin. Buy the ticket on the spot (tell the conductor or driver). You can also buy it online (see below for the Omio link)
+              </figcaption>
+            </figure>
+            <br />
+            <p><strong>{t("venue.venue_transport.air.milan_airport_p7")}</strong></p>
+            <br />
+
             <p><a href="https://www.omio.it/autobus/aeroporto-milano-malpensa-mxp/torino">Milano Malpensa - Torino Porta Susa (Bus)</a></p>
             <br />
             <p>{t("venue.venue_transport.air.milan_bus_disclaimer")}</p>
